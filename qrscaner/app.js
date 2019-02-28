2b575104bfde36b360c8f675f23848e0115608e2
jong2254 <46884141+jong2254@users.noreply.github.com>
2019-02-28 11:37:47 +0800

    Add files via upload

---
+++ qrscaner/app.js
@@ -0,0 +1,28 @@
+var constraints = { video: { facingMode: "enviroment" }, audio: false };
+const cameraView = document.querySelector("#camera--view"),
+      cameraOutput = document.querySelector("#camera--output"),
+      cameraSensor = document.querySelector("#camera--sensor"),
+      cameraTrigger = document.querySelector("#camera--trigger")
+
+function cameraStart() 
+{
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) 
+	{
+		track = stream.getTracks()[0];
+		cameraView.srcObject = stream;
+	})
+	.catch(function(error) 
+	{
+		console.error("Error: ", error);
+	});
+}
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
+//Start the camera when windows load
+window.addEventListener("load", cameraStart, false);
